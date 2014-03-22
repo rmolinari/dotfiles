a1fca404761e071f8609925e3a61c75fcb20cc0e
Rory Molinari <quokka@gmail.com>
2014-03-22 11:15:54 -0400

    Use MELPA

--- init.el
+++ init.el
@@ -19,6 +19,21 @@
 (add-hook 'emacs-lisp-mode-hook 'imenu-elisp-sections)
 
 
+;;;; PACKAGE MANAGEMENT
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  )
+
+
+;;;; APPEARANCE
+
+(load-theme 'wombat t)
+;; (load-theme 'zenburn t)
+
+
 ;;;; MODES
 
 ;; Org mode
