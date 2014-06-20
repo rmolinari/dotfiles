8be3d4723b30e92da9c6cfc1e73e4514e034dc91
Rory Molinari <quokka@gmail.com>
2014-06-20 17:36:05 -0400

    Last changes of the week

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -29,6 +29,8 @@
 
 ;;;; APPEARANCE
 
+(blink-cursor-mode -1)
+
 (when window-system (set-frame-size (selected-frame) 180 55))
 (load-theme 'solarized-dark t)
 ;; (load-theme 'wombat t)
@@ -199,9 +201,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
  '(custom-enabled-themes (quote (wombat)))
  '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
- '(safe-local-variable-values (quote ((eval sh-set-shell "zsh")))))
+ '(safe-local-variable-values (quote ((eval sh-set-shell "zsh"))))
+ '(visible-cursor nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
